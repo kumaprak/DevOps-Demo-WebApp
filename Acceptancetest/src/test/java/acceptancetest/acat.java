2cae2c9b6814e5c40256fe0ec660ef3d7dd87c32
kumaprak <54338656+kumaprak@users.noreply.github.com>
2021-02-14 21:16:15 -0500

    Update acat.java

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -15,7 +15,7 @@ public class acat {
 	public String URL = "http://18.218.99.65:8080/ProdWebapp";
 	public String URL1 = "http://18.218.99.65:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
-	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
+	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp"; 
 	
 	@Test(priority = 0)
 	public void checkin() {
@@ -25,7 +25,7 @@ public class acat {
 		driver.get(URL);
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		// Login Page content check
-		//driver.findElement(By.xpath("//h1[text()='JUPITER COMMUNICATION']")).isDisplayed();
+		//driver.findElement(By.xpath("//h1[text()='JUPITER COMMUNICATION']")).isDisplayed(); Git Poll test
 		driver.findElement(By.name("username")).isDisplayed();
 		driver.findElement(By.name("userpassword")).isDisplayed();
 		driver.findElement(By.xpath("//button[text()='Login']")).isDisplayed();
